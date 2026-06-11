f3b3d6cfd04c5e09b2b56ea449ea8f0c7c3ae751
Felix Holfelder <felixholfelder1@t-online.de>
2026-06-11 21:21:32 +0200

    first dummy version

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,33 +6,22 @@
           <v-card
             class="py-4"
             color="surface-variant"
-            image="https://cdn.vuetifyjs.com/docs/images/one/create/feature.png"
             rounded="lg"
             variant="tonal"
+            @click="resetPrice"
           >
             <template #prepend>
               <v-avatar
                 class="ml-2 mr-4"
-                icon="mdi-rocket-launch-outline"
+                icon="mdi-currency-eur"
                 size="60"
                 variant="tonal"
               />
             </template>
 
-            <template #image>
-              <v-img position="top right" />
-            </template>
-
             <template #title>
               <div class="my-title my-uppercase text-headline-medium font-weight-bold">
-                Get started
-              </div>
-            </template>
-
-            <template #subtitle>
-              <div class="text-body-large">
-                Change this page by updating <v-kbd>{{ `<HelloWorld />` }}</v-kbd> in
-                <v-kbd>components/HelloWorld.vue</v-kbd>.
+                Gesamtpreis: {{ price }} €
               </div>
             </template>
           </v-card>
@@ -40,20 +29,16 @@
 
         <v-col v-for="link in links" :key="link.href" cols="6">
           <v-card
-            append-icon="mdi-open-in-new"
             class="py-4"
             color="surface-variant"
-            :href="link.href"
             rel="noopener noreferrer"
             rounded="lg"
-            :subtitle="link.subtitle"
+            :subtitle="`${link.price} €`"
             target="_blank"
             :title="link.title"
             variant="tonal"
+            @click="addItem(link)"
           >
-            <template #prepend>
-              <v-avatar class="ml-2 mr-4" :icon="link.icon" size="60" variant="tonal" />
-            </template>
           </v-card>
         </v-col>
       </v-row>
@@ -62,30 +47,46 @@
 </template>
 
 <script setup lang="ts">
+import {ref} from "vue";
+
 const links = [
   {
     href: 'https://vuetifyjs.com/',
     icon: 'mdi-text-box-outline',
     subtitle: 'Learn about all things Vuetify in our documentation.',
-    title: 'Documentation',
+    title: 'Bratwurstsemmel',
+    price: 3.5,
   },
   {
     href: 'https://vuetifyjs.com/introduction/why-vuetify/#feature-guides',
     icon: 'mdi-star-circle-outline',
     subtitle: 'Explore available framework Features.',
-    title: 'Features',
+    title: 'Steaksemmel',
+    price: 4.5,
   },
   {
     href: 'https://vuetifyjs.com/components/all',
     icon: 'mdi-widgets-outline',
     subtitle: 'Discover components in the API Explorer.',
-    title: 'Components',
+    title: 'Breze mit Käse',
+    price: 3,
   },
   {
     href: 'https://discord.vuetifyjs.com',
     icon: 'mdi-account-group-outline',
     subtitle: 'Connect with Vuetify developers.',
-    title: 'Community',
+    title: 'Lecker Bierchen',
+    price: 3.5,
   },
 ]
+
+const price = ref(0.0)
+
+const addItem = (item) => {
+  price.value += item.price
+}
+
+const resetPrice = () => {
+  price.value = 0
+}
 </script>
