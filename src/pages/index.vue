8812abfcef8ade257137f7f751e50a16955e845f
Felix Holfelder <felixholfelder1@t-online.de>
2026-06-15 20:45:27 +0200

    added entrypoint for admin overview

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,18 +1,24 @@
 <template>
   <v-container class="fill-height d-flex flex-column justify-center" max-width="1100">
     <div>
+      <v-app-bar title="Kassensystem BV Altenstadt">
+        <template #append>
+          <v-btn color="surface-variant" icon="mdi-cog" @click="$router.push('/admin/login')" />
+        </template>
+      </v-app-bar>
+
       <v-row>
         <v-col cols="12" md="4" sm="12">
           <v-card
             class="py-4"
             color="surface-variant"
-            prepend-icon="mdi-hamburger"
+            prepend-icon="mdi-silverware-fork-knife"
             rel="noopener noreferrer"
             rounded="lg"
             target="_blank"
             title="Essen"
             variant="tonal"
-            @click="$router.push('essen')"
+            @click="$router.push('/essen')"
           />
         </v-col>
 
@@ -26,7 +32,7 @@
             target="_blank"
             title="Ausschank"
             variant="tonal"
-            @click="$router.push('ausschank')"
+            @click="$router.push('/ausschank')"
           />
         </v-col>
 
@@ -40,7 +46,7 @@
             target="_blank"
             title="Schnaps-Bar"
             variant="tonal"
-            @click="$router.push('schnaps-bar')"
+            @click="$router.push('/schnaps-bar')"
           />
         </v-col>
       </v-row>
