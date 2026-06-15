aa213f6e674d3edc09aa3ba8488b67eeaa0d8033
Felix Holfelder <felixholfelder1@t-online.de>
2026-06-15 21:11:59 +0200

    added dialog to create and edit registers

--- src/pages/admin/registers.vue
+++ src/pages/admin/registers.vue
@@ -1,5 +1,76 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import type { Register } from '@/types/register.ts'
+  import { computed, ref } from 'vue'
+  import RegisterDialog from '@/components/dialogs/register-dialog.vue'
 
-<template></template>
+  const items = computed(() => {
+    // TODO - read from database
+    return ref<Register[]>([
+      {
+        id: '1',
+        name: 'Essen',
+      },
+      {
+        id: '2',
+        name: 'Ausschank',
+      },
+      {
+        id: '3',
+        name: 'Kaffee und Kuchen',
+      },
+      {
+        id: '4',
+        name: 'Schnaps-Bar',
+      },
+    ])
+  })
+
+  const isRegisterDialogOpen = ref(false)
+  const selectedItem = ref<Register | null>(null)
+
+  function openRegisterDialog (item: Register | null = null) {
+    selectedItem.value = item
+    isRegisterDialogOpen.value = true
+  }
+
+  function saveItem (item: Register) {
+    console.log(item.id)
+    if (item.id === undefined) {
+    // TODO - save new register
+    } else {
+    // TODO - update existing register
+    }
+  }
+</script>
+
+<template>
+  <div>
+    <register-dialog v-model="isRegisterDialogOpen" :item="selectedItem" @submit="saveItem" />
+
+    <v-app-bar title="Kassen">
+      <template #prepend>
+        <v-btn icon="mdi-arrow-left" @click="$router.back()" />
+      </template>
+
+      <template #append>
+        <v-btn icon="mdi-plus" @click="openRegisterDialog()" />
+      </template>
+    </v-app-bar>
+
+    <v-table class="mt-16">
+      <thead>
+        <tr>
+          <th class="text-left">Name</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="item in items.value" :key="item.id" @click="openRegisterDialog(item)">
+          <td>{{ item.name }}</td>
+        </tr>
+      </tbody>
+    </v-table>
+  </div>
+</template>
 
 <style scoped></style>
---
+++ src/types/register.ts
@@ -0,0 +1,6 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export class Register {
+  id: string = uuidv4()
+  name = ''
+}
