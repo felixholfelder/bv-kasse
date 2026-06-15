70cf21a6c640613371ab247e98fd4178481cae2c
Felix Holfelder <felixholfelder1@t-online.de>
2026-06-15 20:46:42 +0200

    added admin page templates

---
+++ src/pages/admin/events.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+  import { ref } from 'vue'
+  import { useShortDate } from '@/composable/useDates.ts'
+
+  const events = ref([
+    {
+      name: 'Johannisfeuer 2026',
+      date: useShortDate(),
+      activated: true,
+    },
+    {
+      name: 'Waldweihnacht 2025',
+      date: useShortDate(),
+      activated: false,
+    },
+  ])
+</script>
+
+<template>
+  <div>
+    <v-app-bar title="Events">
+      <template #prepend>
+        <v-btn icon="mdi-arrow-left" @click="$router.back()" />
+      </template>
+    </v-app-bar>
+
+    <v-table class="mt-16">
+      <thead>
+        <tr>
+          <th class="text-left">Name</th>
+          <th class="text-left">Datum</th>
+          <th class="text-left">Aktiviert</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="item in events" :key="item.name">
+          <td>{{ item.name }}</td>
+          <td>{{ item.date }}</td>
+
+          <td>
+            <v-switch :value="item.activated" />
+          </td>
+        </tr>
+      </tbody>
+    </v-table>
+  </div>
+</template>
+
+<style scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { auth } from '@/firebase.ts'
+import AdminEvents from '@/pages/admin/events.vue'
 import Admin from '@/pages/admin/index.vue'
 import AdminLogin from '@/pages/admin/login.vue'
+import AdminProducts from '@/pages/admin/products.vue'
+import AdminRegisters from '@/pages/admin/registers.vue'
 import Checkout from '@/pages/checkout.vue'
 import Drinks from '@/pages/drinks.vue'
 import Food from '@/pages/food.vue'
@@ -39,6 +42,24 @@ const routes = [
     path: '/admin',
     name: 'admin',
     component: Admin,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/admin/kassen',
+    name: 'adminRegisters',
+    component: AdminRegisters,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/admin/produkte',
+    name: 'adminProducts',
+    component: AdminProducts,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/admin/events',
+    name: 'adminEvents',
+    component: AdminEvents,
     meta: { requiresAuth: true },
   },
 ]
