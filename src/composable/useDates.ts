a18a5c0e2201f39c7b537435376467ed95969361
Felix Holfelder <felixholfelder1@t-online.de>
2026-06-16 19:12:47 +0200

    added util function to format timestamps

--- src/composable/useDates.ts
+++ src/composable/useDates.ts
@@ -1,3 +1,6 @@
+import type { Timestamp } from 'firebase/firestore'
+import { toRaw } from 'vue'
+
 export function useShortDate () {
   return new Date()
     .toLocaleDateString('de', {
@@ -6,4 +9,13 @@ export function useShortDate () {
       day: '2-digit',
     })
     .replaceAll('.', '')
+}
+
+export function formatTimestamp (timestamp: Timestamp): string {
+  const date = toRaw(timestamp).toDate()
+  return new Date(date).toLocaleDateString('de-DE', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+  })
 }
