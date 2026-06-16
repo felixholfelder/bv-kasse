211c9843de94dc134d74602c87fcc8ce2f64d1ba
Felix Holfelder <felixholfelder1@t-online.de>
2026-06-16 22:32:24 +0200

    added paths for managing events, registers, products

---
+++ src/pages/admin/events/[eventId]/registers/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<!-- TODO - overview over registersmake registers enabled or disabled for every event -->
+
+</template>
+
+<style scoped>
+
+</style>
